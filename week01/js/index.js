c4b1af1b49f24ef2e84169e599a5fe6a28197a33
congcoding <congcoding@gmail.com>
2021-03-17 15:14:17 +0900

    Write poem for week01 homework

--- week01/js/index.js
+++ week01/js/index.js
@@ -1,3 +1,4 @@
-const popAlert = () => {
-    alert('만나서 반가워요.')
+const alertPoet = () => {
+    if (confirm("윤동주 (시에 대한 해설을 보려면 확인을 누르세요)"))
+        location.href = 'https://terms.naver.com/entry.nhn?docId=574297&cid=46645&categoryId=46645';
 }
